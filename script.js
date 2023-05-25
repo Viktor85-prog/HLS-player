051f96b12c37298c1db012ab905485e82b3d1fb1
Victor <bulkin_viktor@mail.ru>
2023-05-25 16:59:49 +0300

    add some more buttons, styles

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (Hls.isSupported()) {
 	hls.loadSource('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
 	hls.attachMedia(video);
 	hls.on(Hls.Events.MEDIA_ATTACHED, function () {
-	  video.muted = true;
+	//   video.muted = true;
 	  video.play();
 	});
   }
