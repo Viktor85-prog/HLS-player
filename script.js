41e137d60b10d76ba6c322edd02bd91322806abb
Victor <bulkin_viktor@mail.ru>
2023-05-25 12:42:58 +0300

    remove logic

---
+++ script.js
@@ -0,0 +1,34 @@
+if (Hls.isSupported()) {
+	var video = document.getElementById('video');
+	var hls = new Hls();
+	hls.loadSource('https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8');
+	hls.attachMedia(video);
+	hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+	  video.muted = true;
+	  video.play();
+	});
+  }
+
+  var video = document.getElementById('video');
+  window.onload = function () {
+	var i = 0;
+	var el = document.getElementById('update');
+
+  function foo() {
+	const r = video.buffered;
+	const pos = video.currentTime;
+	let bufferLen = 0;
+	for (let i = 0; i < r.length; i++) {
+	  if (pos >= r.start(i) && pos < r.end(i)) {
+		  bufferLen = r.end(i) - pos;
+	  }
+	}
+	  el.innerHTML =
+		'<div>currentTime:' + pos + '</div>'+ 
+		'<div>buffered:' + bufferLen + '</div>'+ 
+		'<div>decoded:' + video.webkitDecodedFrameCount + '</div>'+
+		'<div>dropped:' + video.webkitDroppedFrameCount + '</div>';
+	  window.requestAnimationFrame(foo);
+	}
+	foo();
+  };
